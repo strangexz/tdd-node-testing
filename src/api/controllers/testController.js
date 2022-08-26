5e1310b60f513f58d058e895341509ed6f894b83
Strangexz <ja_figueroa@outlook.es>
2022-08-26 10:01:07 -0600

    feat: nueva ruta de controlador

--- src/api/controllers/testController.js
+++ src/api/controllers/testController.js
@@ -1,14 +1,3 @@
-// import HttpStatus from "http-status-codes";
-// import _ from "underscore";
-// import validator from "validator";
-// import * as url from 'url';
-
-// import Logger from '../../config/logger.js';
-// import TestService from "../../services/testService.js";
-
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
 const HttpStatus = require('http-status-codes');
 const _ = require('underscore');
 const validator = require('validator');
@@ -111,7 +100,91 @@ const makeSum = async (req, res) => {
         });
 }
 
+const makeProduct = async (req, res) => {
+
+    // let msg;
+
+    /* Validando el campo  numberOne */
+    // if (!_.isNumber(req.body.numberOne) && !_.isString(req.body.numberOne)) {
+    //     msg = 'el campo "numberOne" debe ser numérico';
+
+    //     log.warn(msg);
+
+    //     return res
+    //         .status(HttpStatus.StatusCodes.BAD_REQUEST)
+    //         .send({
+    //             message: msg,
+    //             result: req.body
+    //         });
+    // }
+
+    // if (_.isString(req.body.numberOne) && !validator.isNumeric(req.body.numberOne)) {
+    //     msg = 'el campo "numberOne" debe ser numérico';
+
+    //     log.warn(msg);
+
+    //     return res
+    //         .status(HttpStatus.StatusCodes.BAD_REQUEST)
+    //         .send({
+    //             message: msg,
+    //             result: req.body
+    //         });
+    // }
+
+    /* Validando el campo  numberTwo */
+    // if (!_.isNumber(req.body.numberTwo) && !_.isString(req.body.numberTwo)) {
+    //     msg = 'el campo "numberTwo" debe ser numérico';
+
+    //     log.warn(msg);
+
+    //     return res
+    //         .status(HttpStatus.StatusCodes.BAD_REQUEST)
+    //         .send({
+    //             message: msg,
+    //             result: req.body
+    //         });
+    // }
+
+    // if (_.isString(req.body.numberTwo) && !validator.isNumeric(req.body.numberTwo)) {
+    //     msg = 'el campo "numberTwo" debe ser numérico';
+
+    //     log.warn(msg);
+
+    //     return res
+    //         .status(HttpStatus.StatusCodes.BAD_REQUEST)
+    //         .send({
+    //             message: msg,
+    //             result: req.body
+    //         });
+    // }
+
+    const numberOne = (_.isNumber(req.body.numberOne)) ? req.body.numberOne : parseFloat(req.body.numberOne);
+    const numberTwo = (_.isNumber(req.body.numberTwo)) ? req.body.numberTwo : parseFloat(req.body.numberTwo);
+
+    const testService = new TestService();
+    const { status, message, result } = await testService.makeProduct(numberOne, numberTwo);
+
+    log.verbose('Devolviendo la respuesta obtenida...');
+
+    if (status === 1) {
+        return res
+            .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+            .send({
+                message,
+                result
+            });
+    }
+
+    return res
+        .status(HttpStatus.StatusCodes.OK)
+        .send({
+            message,
+            result
+        });
+}
+
 module.exports = {
-    makeSum
+    makeSum,
+    makeProduct
 }
 
--- src/api/routes/testRoute.js
+++ src/api/routes/testRoute.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 
 const TestController = require('../controllers/testController');
 
-const api = Router();
+const api = Router()
 
 /* definición de rutas */
 api.post('/makeSum', TestController.makeSum);
+api.post('/makeProduct', TestController.makeProduct);
 
 module.exports = api;
