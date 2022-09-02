dd8ec1a7f8363afe8c0ae8f438ad6d0969b8aeea
Alberto Figueroa <ja_figueroa@outlook.es>
2022-09-01 20:10:54 -0600

    test: pruebas al servicio getDogs

--- src/services/__test__/testService.spec.js
+++ src/services/__test__/testService.spec.js
@@ -1,8 +1,9 @@
 const TestService = require('../testService');
-// const axios = require('axios');
-// const MockAdapter = require('axios-mock-adapter');
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
 
-// let mockAdapter;
+let mockAdapter;
+let baseUrlApi;
 
 describe('Pruebas al servicio Test', () => {
     describe('pruebas a la funcion makeSum', () => {
@@ -82,5 +83,78 @@ describe('Pruebas al servicio Test', () => {
             expect(result).toBeNaN();
         });
 
+    });
+
+    describe('pruebas al método de consultas de apis externas', () => {
+        beforeAll(() => {
+            mockAdapter = new MockAdapter(axios);
+            baseUrlApi = 'https://dog.ceo/api/breeds/image/random';
+        });
+
+        it('deberia de ser una consulta exitosa', async () => {
+            const responseHeader = {
+                'date': new Date().toGMTString(),
+                'content-type': "application/json",
+                'x-powered-by': "PHP/7.3.31",
+                'cache-control': "no-cache, private",
+                'access-control-allow-origin': "*",
+                'via': "1.1 varnish (Varnish/6.3), 1.1 varnish (Varnish/6.3)",
+                'x-cache-hits': 0,
+                'x-cache': "MISS",
+                'age': 0,
+                'vary': "Accept-Encoding",
+                'cf-cache-status': "DYNAMIC",
+                'report-to': {
+                    "endpoints": [
+                        {
+                            "url": "https:/a.nel.cloudflare.com/report/v3?s=fsaOhHpHKYi99SPMpZnFMB8rkPM1prqOB9t0V1o0U7e98NY%2FPURyDGBanBpCfDDNrwIWFdw8ojWcLM16JfdQU8r2cywDNX4ovjO1nxEsRBJsBvd7scR63Kdd"
+                        }
+                    ],
+                    "group": "cf-nel",
+                    "max_age": 604800
+                },
+                'nel': {
+                    "success_fraction": 0,
+                    "report_to": "cf-nel",
+                    "max_age": 604800
+                },
+                'server': "cloudflare",
+                'cf-ray': "74422bdbda19b3da-MIA",
+                'alt-svc': "h3=':443'; ma=86400, h3-29=':443'; ma=86400"
+            };
+
+            const count = 3;
+
+            const res = {
+                status: jest.fn(),
+                result: jest.fn()
+            };
+
+            const mockAxiosResponse = {
+                message: [
+                    "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_3762.jpg",
+                    "https://images.dog.ceo/breeds/weimaraner/n02092339_7596.jpg",
+                    "https://images.dog.ceo/breeds/setter-english/n02100735_4051.jpg"
+                ],
+                status: "success"
+            };
+
+            const urlApi = `${baseUrlApi}/${count}`;
+
+            mockAdapter.onGet(urlApi)
+                .reply(200, mockAxiosResponse, responseHeader);
+
+            const service = new TestService();
+            await service.getDogs(count);
+
+            expect(mockAdapter.history.get.length).toBe(1);
+            expect(mockAdapter.history.get[0].url).toEqual(urlApi);
+            console.log(res.status.mock);
+
+            // expect(res.status.mock.calls).toEqual([
+            //     [0]
+            // ]);
+
+        });
     });
 });
