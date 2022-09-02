b959b1f0c3c0f60152a1f8df83910b9934ce0f2f
Alberto Figueroa <ja_figueroa@outlook.es>
2022-09-01 20:10:39 -0600

    feat: nuevo servicio getDogs

--- src/services/testService.js
+++ src/services/testService.js
@@ -1,6 +1,6 @@
 const _ = require('underscore');
 const Logger = require('../config/logger');
-// const axios = require('axios');
+const axios = require('axios');
 
 const log = Logger(__filename);
 
@@ -94,6 +94,63 @@ class TestServices {
             return { status: 0, message: 'Multiplicacion exitosa', result: product };
 
         } catch (error) {
+            // handle error
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.error('data', error.response.data);
+                console.error('status', error.response.status);
+                console.error('headers', error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.error('request', error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.error('Error', error.message);
+            }
+            console.log(log);
+            log.error(error.message);
+            console.error('config', error.config);
+
+            return { status: 1, message: error.message, result: null };
+        }
+
+    };
+
+    /**
+     * Método que realiza el consumo de un API externo
+     *
+     * Este método se encarga de consumir otra API externa y devolver la respuesta
+     *
+     * @alias getDogs
+     * @param {number} count - numero de imagenes solicitadas
+     * @returns {json} un objeto json con 3 atributos:
+     *  status: numero entero con un código de estado
+     *  message: un mensaje descriptivo de la respuesta
+     *  result: total de la suma.
+     */
+    async getDogs(count) {
+        try {
+            log.info('Inicio del servicio TestServices.getDogs');
+            const urlApi = 'https://dog.ceo/api/breeds/image/random/';
+
+            /** Consultando el servicio de GeoJS */
+            const response = await axios.get(urlApi + count);
+            console.log(response);
+
+            log.debug('response', response);
+            log.debug('response.data', response.data);
+            log.debug('response.config', response.config);
+            log.info('Devolviendo los datos obtenidos de GeoJs... ');
+
+            log.info('Fin del servicio TestServices.getDogs')
+
+            return { status: 0, result: response.data };
+
+        } catch (error) {
+            console.error(error);
             // handle error
             if (error.response) {
                 // The request was made and the server responded with a status code
